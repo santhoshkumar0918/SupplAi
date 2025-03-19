6b1b1a90b24996df39c3e85c64c499b045d79d18
MohamedAshiq09 <mohamedashiq782@gmail.com>
2025-03-19 20:33:56 +0530

    updated the ui in the  batches

--- app/batches/page.tsx
+++ app/batches/page.tsx
@@ -1,3 +1,17 @@
+// "use client";
+
+// import React from "react";
+// import BatchList from "../../components/batches/BatchList";
+
+// export default function BatchesPage() {
+//   return (
+//     <div className="container mx-auto px-4 py-8">
+//       <BatchList />
+//     </div>
+//   );
+// }
+
+
 "use client";
 
 import React from "react";
@@ -5,7 +19,7 @@ import BatchList from "../../components/batches/BatchList";
 
 export default function BatchesPage() {
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 bg-gray-950 min-h-screen">
       <BatchList />
     </div>
   );
